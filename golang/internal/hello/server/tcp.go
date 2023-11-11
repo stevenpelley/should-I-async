d69e176247889f9f1fddc1b5c81ddeb16afdd329
stevenpelley <stevenpelley@gmail.com>
2023-11-10 21:05:14 -0500

    investigated ECONNRESET on first client read Macos allows for only a small TCP accept buffer.  This can quickly fill with pending connections, or established connections that have not been accepted by the application yet.
    Need to:
    have server first respond with a static message allowing the client
    to test.  This can only be done by reading, writes are async.
    retry Dial and read, potentially with sleep and jitter.
    synchronize with sempahore/barrier/wait group for all connections to
    establish.
    add keepalive as some connections will sit idle until others connect.

--- golang/internal/hello/server/tcp.go
+++ golang/internal/hello/server/tcp.go
@@ -4,43 +4,45 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"net"
-	"sync"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func TCPListener(port int) (net.Listener, error) {
 	return net.Listen("tcp", fmt.Sprintf(":%v", port))
 }
 
-func AcceptOrPanic(
+func Accept(
 	ctx context.Context,
-	group *errgroup.Group,
-	wg *sync.WaitGroup,
-	listener net.Listener) error {
-	defer wg.Done()
+	listener net.Listener,
+	errorHandler func(error)) error {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
-		group.Go(func() error {
-			return handleConnection(ctx, conn)
-		})
+		go func() {
+			err := handleConnection(ctx, conn)
+			if err != nil {
+				errorHandler(err)
+			}
+		}()
 	}
 }
 
 // expected to return io.EOF if the client closes connection
 func handleConnection(ctx context.Context, conn net.Conn) error {
 	defer conn.Close()
-	// if there is an error elsewhere the context will be closed and
-	// a blocking call to io.Copy below will return
-	removeAfterFunc := context.AfterFunc(ctx, func() {
+	// if the context is closed we close the connection to unblock any
+	// blocking calls (i.e., io.Copy)
+	defer context.AfterFunc(ctx, func() {
 		conn.Close()
-	})
-	defer removeAfterFunc()
+	})()
 
 	_, err := io.Copy(conn, conn)
 	return err
+}
+
+func LoggingErrorHandler(err error) {
+	slog.Error("handleConnection", "error", err)
 }
--- golang/internal/hello/server/tcp_test.go
+++ golang/internal/hello/server/tcp_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log/slog"
 	"net"
 	"sync"
 	"testing"
@@ -19,10 +20,9 @@ func TestServe(t *testing.T) {
 	require.NoError(err)
 	ctx, cancelTimeoutCtx := context.WithTimeout(context.Background(), dur)
 	defer cancelTimeoutCtx()
-	ctx, cancel := context.WithCancel(ctx)
 	group, ctx := errgroup.WithContext(ctx)
 
-	numClients := 10
+	numClients := 100
 
 	wg1 := new(sync.WaitGroup)
 	wg1.Add(numClients)
@@ -41,62 +41,131 @@ func TestServe(t *testing.T) {
 	defer removeAfterFunc()
 
 	group.Go(func() error {
-		return AcceptOrPanic(ctx, group, wg2, listener)
+		defer wg2.Done()
+		return Accept(ctx, listener, LoggingErrorHandler)
 	})
 
 	for i := 0; i < numClients; i++ {
 		i := i
-		c := make(chan struct{})
 		group.Go(func() error {
-			return client(ctx, wg1, i, c)
+			defer wg1.Done()
+			return client(ctx, i)
 		})
 	}
 
 	wg1.Wait()
 	require.NoError(ctx.Err())
-	cancel()
+	listener.Close()
 	wg2.Wait()
 }
 
-func client(ctx context.Context, wg *sync.WaitGroup, clientIdx int, finished chan struct{}) error {
-	defer wg.Done()
-
+func client(ctx context.Context, clientIdx int) error {
 	conn, err := net.Dial("tcp", ":8080")
 	if err != nil {
+		slog.Error("dial", "error", err)
 		return err
 	}
 	defer conn.Close()
-	// if the test times out or any other client sees an error we'll cancel
-	// all of them.  Closing the connection causes a Write or Read to return.
-	removeAfterFunc := context.AfterFunc(ctx, func() {
+	// close the connection when the context ends to unblock any blocking calls
+	// (i.e., Read or Write)
+	defer context.AfterFunc(ctx, func() {
 		conn.Close()
-	})
-	defer removeAfterFunc()
+	})()
 
 	s := fmt.Sprintf("client %v\n", clientIdx)
 	b := []byte(s)
 	reader := bufio.NewReader(conn)
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		n, err := conn.Write(b)
 		if err != nil {
-			return err
+			slog.Error("client Write", "error", err)
+			return fmt.Errorf("client Write: %w", err)
 		}
 		if n != len(b) {
-			return fmt.Errorf("failed to write entire message: %v", n)
+			err = fmt.Errorf("client Write failed to write entire message: %v", n)
+			slog.Error("failed to write entire message", "error", err)
+			return err
 		}
 
 		readString, err := reader.ReadString('\n')
 		// we don't expect EOF.  Treat it as an error
 		if err != nil {
+			err = fmt.Errorf("client ReadString: %w", err)
+			slog.Error("client ReadString", "error", err)
 			return err
 		}
 		if readString != s {
-			return fmt.Errorf(
-				"expected to read message: %v. received: %v",
+			err = fmt.Errorf(
+				"client ReadString expected to read message: %v. received: %v",
 				s,
 				readString)
+			slog.Error("client ReadString bad message", "error", err)
+			return err
 		}
 	}
 	return nil
+}
+
+// NOTES
+// ECONNRESET appears, on macos to occur because the queue of incoming TCP connection
+// requests has exceeded the limit, which is 5.  On MACOS this includes both connections
+// still in handshaking as well as those just waiting to be Accept()ed.
+//
+// The strange thing is the error on the client comes after Dial() and calls to
+// Write() return without error.  Write() makes sense as it returns once the data
+// reaches the local kernel without any knowledge of the remote host.
+//
+// but I would have expected Dial to return an error.  This calls the connect()
+// syscall under the hood and it's unclear at what point this responds.
+//
+// I suppose the connection could be ACKed and therefore in the ESTABLISHED state
+// but not yet Accept()ed by the remote host, and there's no way for us to
+// distinguish.  But it should not have been able to respond with SYN+ACK in the
+// place, right?
+//
+// questions:
+// at what point does Dial or the connect syscall return?  Must the connection be
+// ESTABLISHED?
+//
+// if it must be ESTABLISHED then does macos use a 2 queue system and end up dropping
+// the ESTABLISHED connection and closing that connection?
+//
+//
+// The test below recreates the issue by not calling Accept for the listener.
+// at 200 clients we observe the ECONNRESET error on the first client read.
+//
+// SOLUTION: retry, potentially with jittered sleeps, on the first read.
+// the server should respond with a fixed message first allowing the client
+// to test the connection.
+// barrier/wait group on all clients connecting, which may mean that we need
+// to add a keepalive via net.ListenConfig
+
+func TestTcpBacklog(t *testing.T) {
+	require := require.New(t)
+
+	dur, err := time.ParseDuration(("5s"))
+	require.NoError(err)
+	ctx, cancelTimeoutCtx := context.WithTimeout(context.Background(), dur)
+	defer cancelTimeoutCtx()
+	group, ctx := errgroup.WithContext(ctx)
+
+	listener, err := TCPListener(8080)
+	require.NoError(err)
+	defer listener.Close()
+
+	numClients := 100
+	wg1 := new(sync.WaitGroup)
+	wg1.Add(numClients)
+
+	for i := 0; i < numClients; i++ {
+		i := i
+		group.Go(func() error {
+			defer wg1.Done()
+			return client(ctx, i)
+		})
+	}
+
+	wg1.Wait()
+	require.NoError(ctx.Err())
 }
